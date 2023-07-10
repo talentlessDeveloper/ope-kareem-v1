7b68e8ed58019ca44c1bd77b17e1a716f7ad8b07
talentlessDeveloper <kareemope52@gmail.com>
2023-07-10 13:57:38 +0100

    FIX: removed seconds from digital clock

--- src/components/DigitalClock.tsx
+++ src/components/DigitalClock.tsx
@@ -9,7 +9,6 @@ const DigitalClock = () => {
   const options: Intl.DateTimeFormatOptions = {
     hour: '2-digit',
     minute: '2-digit',
-    second: '2-digit',
     hour12: true,
   };
   useInterval(tick, 1000);
