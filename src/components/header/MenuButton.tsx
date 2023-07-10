5f6f0ae8977d0a44f77f670a07d7c76a2070047d
talentlessDeveloper <kareemope52@gmail.com>
2023-07-10 12:51:18 +0100

    FIX: missing type was included

--- src/components/header/MenuButton.tsx
+++ src/components/header/MenuButton.tsx
@@ -1,9 +1,13 @@
-/* eslint-disable react/prop-types */
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-const MenuButton = ({ openMenu, setOpenMenu }) => {
+type MenuButtonProps = {
+  openMenu: boolean;
+  setOpenMenu: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const MenuButton = ({ openMenu, setOpenMenu }: MenuButtonProps) => {
   return (
-    <button className='space-y-1' onClick={() => setOpenMenu((m) => !m)}>
+    <button className="space-y-1" onClick={() => setOpenMenu((m) => !m)}>
       <motion.span
         animate={
           openMenu ? { y: 10, rotate: [-360, -45] } : { y: 0, rotate: 360 }
@@ -12,7 +16,7 @@ const MenuButton = ({ openMenu, setOpenMenu }) => {
           ease: [0.6, 0.01, -0.05, 0.95],
           duration: 0.45,
         }}
-        className='h-[2px] w-8 bg-black block'
+        className="h-[2px] w-8 bg-black block"
       ></motion.span>
       <motion.span
         animate={openMenu ? { rotate: [360, 45] } : { rotate: -360 }}
@@ -20,15 +24,15 @@ const MenuButton = ({ openMenu, setOpenMenu }) => {
           ease: [0.6, 0.01, -0.05, 0.95],
           duration: 0.45,
         }}
-        className='h-[2px] w-7 bg-black block'
+        className="h-[2px] w-7 bg-black block"
       ></motion.span>
       <motion.span
         animate={openMenu ? { y: -10, opacity: 0 } : { y: 0, opacity: 1 }}
         transition={{
-          ease: "easeInOut",
+          ease: 'easeInOut',
           duration: 0.45,
         }}
-        className='h-[2px] w-5 bg-black block'
+        className="h-[2px] w-5 bg-black block"
       ></motion.span>
     </button>
   );
